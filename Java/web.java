a6ee3b6e707040f8770fde6d8c273bab6c5ddf4e
Bing Pei <bpei@vmware.com>
2016-05-04 18:36:07 +0800

    update

--- Java/web.java
+++ Java/web.java
@@ -14,4 +14,56 @@ Dispatch<SOAPMessage> dispatch=service.createDispatch(new QName(ns,"MyServiceImp
 e. 创建SOAP消息
 	i. 用MessageFactory新建SOAPMessage
 	ii. 从SOAPMessage中get到Body
-	iii. 往body中addChildElement，即
+	iii. 往body中addChildElement
+f. Dispatch inovke消息，返回response
+g. 处理response
+
+import java.net.URL;
+import javax.xml.namespace.QName;
+import javax.xml.soap.MessageFactory;
+import javax.xml.soap.SOAPBody;
+import javax.xml.soap.SOAPBodyElement;
+import javax.xml.soap.SOAPConstants;
+import javax.xml.soap.SOAPEnvelope;
+import javax.xml.saop.SOAPMessage;
+import javax.xml.ws.Dispatch;
+import javax.xml.ws.Service;
+import org.w3c.dom.Document;
+
+public class Receive {
+	@SuppressWarnings("unused")
+	public static void main(String[] args) throws Exception {
+            URL url=new URL(wsdlUrl); 
+            QName qname=new QName(ns,"MyServiceImplService"); 
+            Service service=Service.create(url,qname); 
+              
+//          2，创建Dispicathc 
+              
+            Dispatch<SOAPMessage> dispatch=service.createDispatch(new QName(ns,"MyServiceImplPort"), 
+                    SOAPMessage.class, Service.Mode.MESSAGE); 
+//          3，创建消息 
+            SOAPMessage message=MessageFactory.newInstance().createMessage(); 
+            SOAPBody body =message.getSOAPPart().getEnvelope().getBody(); 
+            SOAPElement ele=body.addChildElement(new QName(ns,"add","nn")); 
+            ele.addChildElement("number1").setValue("12"); 
+            ele.addChildElement("number2").setValue("13"); 
+              
+            SOAPMessage response=dispatch.invoke(message); 
+            //response.writeTo(System.out); 
+            NodeList nodeList=response.getSOAPBody().getElementsByTagName("result"); 
+            Node node=nodeList.item(0); 
+            System.out.println(node.getTextContent()); 
+	}
+}
+
+QName:
+<wsdl:definitions name="Helloworld" targetNamespace="http://server.com/" xmlns:ns1="http://schemas.xmlsoap.org/soap/http" xmlns:soap="http://schemas.xmlsoap.org/wsdl/soap/" xmlns:tns="http://server.com/" xmlns:wsdl="http://schemas.xmlsoap.org/wsdl/" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+.....
+<wsdl:portTypename="IHelloWorldService">
+<wsdl:operation name="sayHello">
+<wsdl:inputmessage="tns:sayHello" name="sayHello" />
+<wsdl:outputmessage="tns:sayHelloResponse" name="sayHelloResponse" />
+</wsdl:operation>
+</wsdl:portType>
+</wsdl:definitions>
+以wsdl:portType为例：wsdl是名字空间前缀，portTpye是元素名称，wsdl:portType就是一个qname，其namespace是：http://server.com/
